40483c50d60bc70b350005efd36ed548db8b9d60
Andreas Eich <andi@MacBook-Pro-von-Andreas.local>
2024-05-28 20:39:22 -1000

    analysis chl a vs indices

--- physio/scripts/functions.R
+++ physio/scripts/functions.R
@@ -290,17 +290,17 @@ get_info <- function(data){
       spec = substr(sample_id, 4,4),
       size = substr(sample_id, 5,5),
       replicate = as.numeric(substr(sample_id, 6,6))) %>% 
-    mutate(site = recode(site,
+    mutate(site = dplyr::recode(site,
                          "T" = "Temae",
                          "E" = "E2B"),
-           depth = recode(depth,
+           depth = dplyr::recode(depth,
                           "D" = "20 m",
                           "S" = "5 m"),
-           spec = recode(spec,
+           spec = dplyr::recode(spec,
                          "A" = "A. hya",
                          "P" = "P. ver",
                          "M" = "P. mea"),
-           size = recode(size,
+           size = dplyr::recode(size,
                          "L" = "Adult",
                          "S" = "Juvenile"))
 }
